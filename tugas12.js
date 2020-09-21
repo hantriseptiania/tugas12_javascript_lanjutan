17c4614e227681c2a6a968436f3a091763acb7f4
hantriseptiania <71365751+hantriseptiania@users.noreply.github.com>
2020-09-21 08:44:16 +0700

    Add files via upload

---
+++ tugas12.js
@@ -0,0 +1,6 @@
+var dataMAhasiswa = ["Pisang", "Jeruk", "Apel", "Mangga"]
+console.log(dataMAhasiswa)
+dataMAhasiswa.pop()
+console.log(dataMAhasiswa)
+dataMAhasiswa.shift()
+console.log(dataMAhasiswa)
